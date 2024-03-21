23888211d1c7ebbdeb3cf8aa60e99465c161b3af
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-03-21 17:36:56 +0100

    SA

--- src/component/DownloadCVButton.jsx
+++ src/component/DownloadCVButton.jsx
@@ -3,20 +3,12 @@ import "../Styles.scss";
 const DownloadCVButton = () => {
   return (
     <>
-      <a
-        href="/Falola-Tosin's-Resume.PDF"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a target="_blank" href="/Falola-Tosin's-Resume.PDF">
         <div style={{ cursor: "pointer" }} className="resume">
           View Resume
         </div>
       </a>
-      <a
-        href="/Falola-Tosin's-Resume.PDF"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a target="_blank" href="/Falola-Tosin's-Resume.PDF">
         <div style={{ cursor: "pointer" }} className="resumes">
           View Resume
         </div>
