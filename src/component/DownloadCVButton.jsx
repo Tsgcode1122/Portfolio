f2196c410f20a2db5b8ba8bd71c7029bf1fbdad1
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-04-16 18:47:45 +0100

    u

--- src/component/DownloadCVButton.jsx
+++ src/component/DownloadCVButton.jsx
@@ -5,15 +5,18 @@ const DownloadCVButton = () => {
     <>
       <a
         target="_blank"
-        // href="https://drive.google.com/file/d/1UJE0vzPOepSFCGFMbkdwdL33j_y4VZEq/view?usp=sharing"
-        href=""
+        href="https://drive.google.com/file/d/1xbxABaJ7c8ZDNfDGIrC1-c587crqov3V/view?usp=drivesdk"
         type="application/pdf"
       >
         <div style={{ cursor: "pointer" }} className="resume">
           View Resume
         </div>
       </a>
-      <a target="_blank" href="" type="application/pdf">
+      <a
+        target="_blank"
+        href="https://drive.google.com/file/d/1xbxABaJ7c8ZDNfDGIrC1-c587crqov3V/view?usp=drivesdk"
+        type="application/pdf"
+      >
         <div style={{ cursor: "pointer" }} className="resumes">
           View Resume
         </div>
